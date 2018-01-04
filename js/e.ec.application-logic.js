60d0d074365877209c3ac5e570e8f746f1f7333b
Daynis Olman <daynis@ditconsulting.com.au>
2018-01-04 12:21:29 +1100

    First 2 Click Simulations Added to Logic

--- js/e.ec.application-logic.js
+++ js/e.ec.application-logic.js
@@ -1,1 +1,60 @@
-// Enhanced E-Commerce Application Logic
+//Setting up
+window.onload = function() {
+    addListeners();
+};
+
+//Define Event Listeners
+
+function addListeners(){
+  document.getElementById("e-ec-testButtonA").addEventListener('click', simulateProductClick, false);
+  document.getElementById("e-ec-testButtonB").addEventListener('click', simulateCheckoutComplete, false);
+}
+
+//Simulating Product Click
+function simulateProductClick() {
+window.alert("Starting Simulating Product Click");
+dataLayer.push({
+    'event': 'productClick',
+    'ecommerce': {
+        'currencyCode': 'AUD', 
+        'click': {
+            'actionField': {'list': '/weight-loss'}, // Same as impression location
+            'products': [{
+                'name': 'Book Consultation',
+                'id': '1',
+                'price': '1119.88',
+                'brand': 'Jenny Craig',
+                'category': 'lead',
+                'variant': '',
+                'position': 1
+}]}}}); 
+window.alert("Simulating Product Click Completed");
+}
+
+//Simulating Checkout Complete
+function simulateCheckoutComplete() {
+window.alert("Starting Checkout Complete Simulation");
+dataLayer.push({
+    'event': 'purchase',
+    'ecommerce': {
+        'currencyCode': 'AUD', 
+        'purchase': {
+            'actionField': {
+                'id': 'T12345',
+                'affiliation': undefined, 
+                'revenue': '',
+                'tax':'',
+                'shipping': '2000.00',
+                'coupon': 'NewYear'
+                },
+                'products': [{
+                    'name': 'Book Consultation',
+                    'id': '1',
+                    'price': '999.83',
+                    'brand': 'Jenny Craig',
+                    'category': 'lead',
+                    'quantity': 1,
+                    'variant': ''
+}]}}});
+window.alert("Finished Simulating Checkout-Complete");
+}
